8d9dad9f9fa09fa3ea8084bf3e3f4ea1739418d8
shahjalal <rjshahjalal132@gmail.com>
2024-09-15 17:10:03 +0600

    added settings link beside plugin

--- inc/classes/class-admin-menu.php
+++ inc/classes/class-admin-menu.php
@@ -2,8 +2,11 @@
 
 namespace BOILERPLATE\Inc;
 
+defined( "ABSPATH" ) || exit( "Direct Access Not Allowed" );
+
 use BOILERPLATE\Inc\Traits\Program_Logs;
 use BOILERPLATE\Inc\Traits\Singleton;
+
 
 class Admin_Menu {
 
@@ -15,7 +18,11 @@ class Admin_Menu {
     }
 
     public function setup_hooks() {
+        // Add admin menu
         add_action( 'admin_menu', [ $this, 'admin_menu_callback' ] );
+
+        // Add settings link to the plugin page
+        add_filter( 'plugin_action_links_' . PLUGIN_BASENAME, [ $this, 'add_settings_link' ] );
     }
 
     public function admin_menu_callback() {
@@ -23,7 +30,7 @@ class Admin_Menu {
             'awpiq_settings',
             'AWPIQ Settings',
             'manage_options',
-            'wporg_options_page',
+            'awpiq_options_page',  // Make sure the slug is consistent
             [ $this, 'awpiq_settings_page_html' ],
             'dashicons-admin-generic',
             45
@@ -32,10 +39,15 @@ class Admin_Menu {
 
     public function awpiq_settings_page_html() {
         ?>
-
         <h1><?php _e( 'AWPIQ Settings', 'intakq' ); ?></h1>
-
         <?php
     }
 
+    // Add the settings link to the plugin page
+    public function add_settings_link( $links ) {
+        // Add the custom settings link to your AWPIQ settings page
+        $settings_link = '<a href="admin.php?page=awpiq_options_page">' . __( 'Settings', 'intakq' ) . '</a>';
+        array_unshift( $links, $settings_link );
+        return $links;
+    }
 }
