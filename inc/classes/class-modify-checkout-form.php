5e27f6ec81a1f736a8f0ddebdade38cbcf30a1c4
shahjalal <rjshahjalal132@gmail.com>
2024-09-15 16:26:28 +0600

    unset company and billing address

--- inc/classes/class-modify-checkout-form.php
+++ inc/classes/class-modify-checkout-form.php
@@ -15,8 +15,22 @@ class Modify_Checkout_Form {
     }
 
     public function setup_hooks() {
-        // Actions and filters hooks
-        
+        add_filter( 'woocommerce_checkout_fields', [ $this, 'custom_override_checkout_fields' ] );
     }
 
+    public function custom_override_checkout_fields( $fields ) {
+
+        // Unset the company name field
+        unset( $fields['billing']['billing_company'] );
+
+        // Unset billing address fields
+        unset( $fields['billing']['billing_address_1'] );
+        unset( $fields['billing']['billing_address_2'] );
+        unset( $fields['billing']['billing_city'] );
+        unset( $fields['billing']['billing_postcode'] );
+        unset( $fields['billing']['billing_state'] );
+        unset( $fields['billing']['billing_country'] );
+
+        return $fields;
+    }
 }
